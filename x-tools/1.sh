bca8b3c7ff7f0b46144be67988b2ba91cdea2774
xiongzhuosen <xiongzhuosen@gmail.com>
2026-01-19 19:35:33 +0800

    5

--- x-tools/1.sh
+++ x-tools/1.sh
@@ -1,5 +1,5 @@
 wget https://developer.arm.com/-/media/Files/downloads/gnu/15.2.rel1/binrel/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz
 sudo apt update
 sudo apt install build-essential cmake -y
-tar -zxvf arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz
+tar -xvf arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz
 mv arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-linux-gnueabihf arm-none-linux-gnueabihf
